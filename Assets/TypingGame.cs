348e1c4a67562df14a852ddfc17b8074ba32eeb1
alexandre-jme1234 <alexandre@coliback.com>
2025-03-08 17:35:05 +0100

    logique mini-jeux update av Dico valeur

--- Assets/TypingGame.cs
+++ Assets/TypingGame.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Runtime.ExceptionServices;
+using Unity.VisualScripting;
+using JetBrains.Annotations;
 
 public class TypingGame : MonoBehaviour
 {
@@ -10,15 +13,24 @@ public class TypingGame : MonoBehaviour
     public TMP_InputField inputField;
     public TMP_Text resultText;
     public Button enterButton;
+    Dictionary<int, string> consignes = new Dictionary<int, string>();
 
     public string text;
     private float timeLimit = 100000000000000f;
     private float timeRemaining;
-    private bool isGameActive = false;
+    private bool isGameActive = false; 
+
 
     void Start()
     {
         enterButton.onClick.AddListener(CheckInput);
+
+        consignes.Add(1, "schrifblokrbsts");
+        consignes.Add(2, "laxydbzwo");
+        consignes.Add(3, "soutien");
+
+        consigneText.text = consignes[1];
+
         StartGame();
     }
 
@@ -31,13 +43,18 @@ public class TypingGame : MonoBehaviour
             {
                 EndGame(false);
             }
+
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                
+                CheckInput();
+            }
         }
     }
 
 
     void StartGame()
     {
-
         timeRemaining = timeLimit;
         inputField.interactable = true;
         isGameActive = true;
@@ -46,22 +63,34 @@ public class TypingGame : MonoBehaviour
     }
 
     void CheckInput()
-    {
-        string[] words = { "TESTER" };
-        text = words[Random.Range(0, words.Length)];
-
-        if(inputField.text == text)
+    {     
+        if (inputField.text.Length == consigneText.text.Length)
         {
-            EndGame(true);
-        } else {
+            bool isMatching = true;
+
+            for (int i = 0; i < consigneText.text.Length; i++)
+            {
+                if (inputField.text[i] != consigneText.text[i])
+                {
+                    isMatching = false;  
+                    break;
+                }
+            }
+
+            if (isMatching)
+            {
+                EndGame(true);
+            }
+    } else {
+        {
             EndGame(false);
         }
+    }
     }
 
     void EndGame(bool isWin)
     {
-        inputField.interactable = false;
-
+        inputField.interactable = true;
         if (isWin)
         {
             resultText.text = "Gagné !";
