ac170d38752baea9ba284f82f3eeca97c33163ce
Théo KOKEL <kokelsan@outlook.com>
2025-03-09 02:46:54 +0100

    added door animation playing on game started

---
+++ Assets/Scripts/Managers/EnvironmentManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class EnvironmentManager : MonoBehaviour
+{
+    public Animator DoorAnimator;
+    public string DoorAnimationStateName = "DoorOpening";
+
+    void Start()
+    {
+        GameManagerHandlerData.OnGameStarted += OnGameStarted;
+    }
+
+    private void OnDestroy()
+    {
+        GameManagerHandlerData.OnGameStarted -= OnGameStarted;
+    }
+
+    private void OnGameStarted()
+    {
+        DoorAnimator.Play(DoorAnimationStateName);
+    }
+}
